20cdd3f15a1e8611894c4b5227fa4de406d4a3f4
zeushera140 <510895877@qq.com>
2025-03-02 23:01:38 +0800

    Update chat.go

--- chat.go
+++ chat.go
@@ -24,12 +24,6 @@ const (
 	userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.79"
 )
 
-type Options struct {
-    Retry  int
-    Model  string
-    Mode   string // Add this new field
-    jar    http.CookieJar
-}
 
 type webClaude2Response struct {
 	Id           string `json:"id"`
